27da29988a71dec445740b6c481b69cb3d548d94
FarmFinder <114182668+agrorithms@users.noreply.github.com>
2026-07-13 18:24:54 -0400

    enabled insurrection prime

--- src/app/player/[membershipType]/[membershipId]/PlayerProfileClient.tsx
+++ src/app/player/[membershipType]/[membershipId]/PlayerProfileClient.tsx
@@ -102,7 +102,7 @@ interface ActiveSessionResponse {
 const PRIVATE_DATA_MESSAGE = 'The user has chosen for this data to be private. Data may be incomplete';
 const AVAILABLE_RAIDS: RaidOption[] = [
     //pantheon insurrection prime and morgeth surpassing are not accurately showing fresh clears so will never return results
-    //{ key: 'pantheon_insurrection_prime_revolutionary', name: "Pantheon: Insurrection Prime Revolutionary" },
+    { key: 'pantheon_insurrection_prime_revolutionary', name: "Pantheon: Insurrection Prime Revolutionary" },
     //{ key: 'pantheon_morgeth_surpassing', name: 'Pantheon: Morgeth Surpassing' },
     { key: 'pantheon_calus_resplendent', name: 'Pantheon: Calus Resplendent' },
     { key: 'the_desert_perpetual', name: 'The Desert Perpetual' },
